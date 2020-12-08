4fca1c04aea955fa1ea6514538670436c3ac2d79
blminh <0306181245@caothang.edu.vn>
2020-12-08 22:55:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.trang-chu');
-});
-
-Route::get('dang-nhap', function () {
-
-    return view('login');
-})->name('dang-nhap');
-
-Auth::routes();
-
-Route::group(['prefix'=>'phim'], function () {
-    Route::get('danh-sach-phim', 'HomeController@danh_sach_phim')->name('danh-sach-phim');
-
-    Route::get('them-phim', 'HomeController@them_phim')->name('them-phim');
-
-    Route::get('cap-nhat-phim', 'HomeController@cap_nhat_phim')->name('cap-nhat-phim');
-});
-
-Route::group(['prefix'=>'rap-phim'], function () {
-    Route::get('danh-sach-rap', 'HomeController@danh_sach_rap')->name('danh-sach-rap');
-});
-
-Route::group(['prefix'=>'thanh-vien'], function () {
-    Route::get('danh-sach-thanh-vien', 'HomeController@danh_sach_thanh_vien')->name('danh-sach-thanh-vien');
+    return view('welcome');
 });
